f640fad9a53cc325474d2944d2f8190a40500c5c
Austinsahm <austinsamuel733@gmail.com>
2025-01-02 23:57:32 +0100

    Edited

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-// import { Link } from "react-router-dom";
 import { User } from "@/types/types";
 import { DataTable } from "@/components/DataTable";
 import { columns } from "@/components/columns";
